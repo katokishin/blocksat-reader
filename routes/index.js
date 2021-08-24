b35b94b2e0166b4e8d190a87715d0f47dd544b5d
Kishin Kato <kishin@trustless-services.com>
2021-08-24 12:06:30 -0700

    RSS feed added

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,16 @@ const router = express.Router();
 require('dotenv').config()
 const Entry = require('../models/entry')
 
+let RSS = require('rss');
+const entry = require('../models/entry');
+
 /* GET home page. */
 router.get('/', async (req, res, next) => {
   try {
     // Get list of 30 recent entries
     new Entry().orderBy('name', 'DESC').fetchPage({page:1, pageSize:30})
       .then(entries => {
-        console.log(entries.serialize())
+        // console.log(entries.serialize())
         res.render('index', { title: 'Blocksat reader', fileList: entries.serialize() });
       })
       .catch(err => {
@@ -20,5 +23,46 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+/* GET RSS feed - NOTE: configured here solely for blocksat-reader.herokuapp.com */
+router.get('/rss.xml', async (req, res, next) => {
+  try {
+    // TODO: implement caching
+    // Create feed
+    let feed = new RSS({
+      title: 'Latest Blockstream Satellite transmissions',
+      descriptions: 'The 30 latest transmissions received via Blocksat',
+      feed_url: 'https://blocksat-reader.herokuapp.com/rss.xml',
+      site_url: 'https://blocksat-reader.herokuapp.com',
+      webMaster: '@btc_dakara on Twitter'
+    })
+
+    // Get list of 30 recent entries & add them to feed
+    new Entry().orderBy('name', 'DESC').fetchPage({page:1, pageSize:30})
+      .then(entries => {
+        // console.log(entries.serialize())
+        entries.serialize().forEach(entry => {
+          feed.item({
+            title: `${entry.name}, type: ${entry.type}`,
+            description: entry.text,
+            url: entry.url,
+            date: nameToDate(entry.name)
+          })
+        })
+        res.send(feed)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+/* Helper: entry name to Date */
+const nameToDate = name => {
+  return new Date(name.substring(0,4) + '-' + name.substring(4,6) + '-' + name.substring(6,8) + ' '
+          + name.substring(8,10) + ':' + name.substring(10,12) + ':' + name.substring(12,14)
+          + ' +0900')
+}
 
 module.exports = router
