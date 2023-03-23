33363b713873b7666e26c407e2d057a06d203997
Kishin Kato <kishin@trustless-services.com>
2023-03-23 17:29:06 +0000

    No more SSL on db; db is local

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const knex = require('knex')({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false }
+        ssl: false
     }
 })
 
